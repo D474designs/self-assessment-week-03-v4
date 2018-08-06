0feca22591b1758d839ebc91d757688e70ef3e8f
D474th3D357R0Y3R <jcazares117@gmail.com>
2018-08-06 09:52:36 -0500

    add example syntax, and pseudo-code to angularjs-pond

--- angularjs-pond/README.md
+++ angularjs-pond/README.md
@@ -2,8 +2,8 @@
 
 Create a fish pond in AngularJS:
 
-* [ ] Install this prompts dependencies by running `npm install` from the root of this directory
-* [ ] Start the app by running `npm start` from the root of this directory
+* [x] Install this prompts dependencies by running `npm install` from the root of this directory
+* [x] Start the app by running `npm start` from the root of this directory
 * [ ] Refactor the `fishTable` and `fishTableRow` components to dynamically render the data in `fishData`
 * [ ] Modify `fishTableRow` such that visibility of the description toggles when a fish is clicked. The description should start out not visible.
 
--- angularjs-pond/src/components/fishTableRow.js
+++ angularjs-pond/src/components/fishTableRow.js
@@ -5,11 +5,53 @@ angular.module('fish-pond')
     this.showDescription = !this.showDescription;
   };
 })
+
+// <div class="container-fluid">
+//   <div class="row">
+//     <div class="col-md-2">
+//       <!--Sidebar content-->
+//
+//       <p>
+//         Search:
+//         <input ng-model="$ctrl.query">
+//       </p>
+//
+//       <p>
+//         Sort by:
+//         <select ng-model="$ctrl.orderProp">
+//           <option value="name">Alphabetical</option>
+//           <option value="age">Newest</option>
+//         </select>
+//       </p>
+//
+//     </div>
+//     <div class="col-md-10">
+//       <!--Body content-->
+//
+//       <ul class="phones">
+//         <li ng-repeat="phone in $ctrl.phones | filter:$ctrl.query | orderBy:$ctrl.orderProp">
+//           <span>{{phone.name}}</span>
+//           <p>{{phone.snippet}}</p>
+//         </li>
+//       </ul>
+//
+//     </div>
+//   </div>
+// </div>
+
+// Refactor to dynamically display main.js fishData
+
 .component('fishTableRow', {
   bindings: {
     fish: '<'
   },
   controller: 'FishTableCtrl',
+  //       <ul class="phones">
+  //         <li ng-repeat="phone in $ctrl.phones | filter:$ctrl.query | orderBy:$ctrl.orderProp">
+  //           <span>{{phone.name}}</span>
+  //           <p>{{phone.snippet}}</p>
+  //         </li>
+  //       </ul>
   template: `
     <div ng-click="$ctrl.toggleDescription()">
       <span class="fish-name">Nemo</span>
