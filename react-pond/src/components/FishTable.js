b8a7a9259e6384ec8eddc7ea3fffd5b2c9f6569c
D474th3D357R0Y3R <jcazares117@gmail.com>
2018-08-06 09:25:37 -0500

    add pseudo-code, and example syntax to react-pond

--- react-pond/src/components/FishTable.js
+++ react-pond/src/components/FishTable.js
@@ -1,5 +1,31 @@
 var FishTable = () => (
   <table>
+
+    // function App1() {
+    //   return <Greeting firstName="Ben" lastName="Hector" />;
+    // }
+    //
+    // function App2() {
+    //   const props = {firstName: 'Ben', lastName: 'Hector'};
+    //   return <Greeting {...props} />;
+    // }
+
+    //     const Button = props => {
+    //   const { kind, ...other } = props;
+    //   const className = kind === "primary" ? "PrimaryButton" : "SecondaryButton";
+    //   return <button className={className} {...other} />;
+    // };
+    //
+    // const App = () => {
+    //   return (
+    //     <div>
+    //       <Button kind="primary" onClick={() => console.log("clicked!")}>
+    //         Hello World!
+    //       </Button>
+    //     </div>
+    //   );
+    // };
+
     // Display proper information from index.js
     <tbody>
       <FishTableRow />
